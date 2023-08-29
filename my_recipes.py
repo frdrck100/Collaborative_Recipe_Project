63a1d837c357a01422763a7168932c1485e42292
frdrck100 <142630802+frdrck100@users.noreply.github.com>
2023-08-29 23:10:26 +0000

    Update my_recipes.py

--- my_recipes.py
+++ my_recipes.py
@@ -10,17 +10,23 @@ def add_recipe(recipe_name, ingredients, instructions):
     recipe_book[recipe_name] = recipe
 
 # Add recipes to the recipe book
-add_recipe("Ghanaian Jollof Rice", ["2 cups rice", "1 cup tomato sauce", 
-            "2 onion"], "1. Cook rice.\n2. Mix with tomato sauce and onion.")
+def main():
+    while True:
+        print("\n1. Add a new recipe")
+        print("2. Exit")
+        choice = input("Enter your choice: ")
 
-add_recipe("Banana Pancakes", ["2 ripe bananas", "1 cup flour", "1 egg"],
-           "1. Mash bananas.\n2. Mix with flour and egg.\n3. Cook as pancakes.")
+        if choice == "1":
+            recipe_name = input("Enter recipe name: ")
+            ingredients = input("Enter ingredients (comma-separated): ").split(",")
+            instructions = input("Enter instructions (one per line, end with an empty line):\n")
+            add_recipe(recipe_name, ingredients, instructions)
+            print("Recipe added successfully!")
+        elif choice == "2":
+            break
+        else:
+            print("Invalid choice. Try again.")
 
-# Print the recipe book
-for recipe_name, recipe in recipe_book.items():
-    print(f"Recipe: {recipe_name}")
-    print("Ingredients:", ", ".join(recipe["ingredients"]))
-    print("Instructions:")
-    for step_num, step in enumerate(recipe["instructions"].split("\n"), start=1):
-        print(f"Step {step_num}: {step}")
-    print("=" * 30)
+# Run the main function
+if __name__ == "__main__":
+    main()
